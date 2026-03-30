7b702fcbc5a973707158141833dcc36aee137436
Vishal S. Pandey <bishalpandey2001@gmail.com>
2026-03-30 12:43:12 +0530

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   url: 'https://deepbrain-labs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/variational-linear-attention/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
